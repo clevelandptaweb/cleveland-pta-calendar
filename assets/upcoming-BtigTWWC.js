cdc1b2bedc6361e17acba46914dda4be095cf57f
clevelandptaweb <clevelandptaweb@gmail.com>
2026-07-15 18:49:44 +0000

    Updates

--- assets/upcoming-BtigTWWC.js
+++ assets/upcoming-BtigTWWC.js
@@ -1,4 +1,4 @@
-import{r as e}from"./google-Dn9YpXej.js";async function t(){let t=(await e()).slice(0,3);document.querySelector(`#app`).innerHTML=`
+import{r as e}from"./google-CwGy3ubr.js";async function t(){let t=(await e()).slice(0,3);document.querySelector(`#app`).innerHTML=`
 <div class="home-upcoming">
 
 <h1>UPCOMING EVENTS</h1>
